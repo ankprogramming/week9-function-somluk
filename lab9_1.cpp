1155171a2f3625a8901e06c38193bf0203e5fc2c
Unknown <somluk2792@gmail.com>
2018-01-29 18:20:59 +0700

    ทำเสร็จแล้วค่ะ

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,13 +2,39 @@
 
 using namespace std;
 
+void swap(int*a, int*b){
+    int bow;
+    bow = *a;
+    *a = *b;
+    *b = bow;
+    }
+
 int main() {
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
     */
+    int a,b,cmd;
+    cin >> a;
+    cin >> b;
+    do {
+    cout << "MENU" << endl;
+    cout << "1. swap( )" << endl;
+    cout << "2. Quit( )" << endl;
+    cin >> cmd;
 
+        if (cmd == 1){
+            swap(&a,&b);
+            cout << a << endl;
+            cout << b << endl;
+    }else{
+        break;
+    }
 
-
-    return 0;
+    } while( 1 );
 }
+
+
+
+
+
